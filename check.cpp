3064b3f5ca4b5a2a4095b28a829ca793989ee0ee
wenzihan <45282468+wenzihan@users.noreply.github.com>
2018-12-18 15:35:45 +0800

    Add check.h&check.cpp
    create a class named "check"
    package the check of command line input

---
+++ check.cpp
@@ -0,0 +1,90 @@
+#include"stdafx.h"
+#include"sudoku.h"
+#include"check.h"
+#include<algorithm>
+#include<stdio.h>
+	
+    check::check(int argc ,char *argv[])
+	{
+		this->argc = argc;
+		this->argv = argv;
+		filename = NULL;
+		type = 0;
+		num = 0;
+		input_error = false;
+	}
+	void check::input_type_analyse()
+	{
+		if (argc == 3)
+		{
+			if (strcmp(argv[1], "-c") == 0)
+			{
+				type = 'c';
+				int len = strlen(argv[2]);
+				num = 0;
+				int flag = 0;
+				for (int i = 0; i < len; i++)
+				{
+					if (argv[2][i] >= '0'&&argv[2][i] <= '9')
+						num = num * 10 + (argv[2][i] - '0');
+					else
+					{
+						input_error = true;
+						printf("Error found in parameter, you should input only integar like '0~9'\n");
+						num = -1;
+						flag = 1;
+						break;
+					}
+				}
+				if (flag==0&&(num <= 0 || num > 1000000))
+				{
+					num = -1;
+					input_error = true;
+					printf("Number of sudoku wrong !\n");
+				}
+			}
+			else if (strcmp(argv[1], "-s") == 0)
+				{
+					type = 's';
+					filename = argv[2];
+
+					//prevent us from opening a file that doesn't exist
+					errno_t err;
+					FILE *tryopen;
+					err = fopen_s(&tryopen, filename, "r");
+					if (err != 0)
+					{
+						input_error = true;
+						printf("The file you want to open doesn't exist\n");
+					}
+				}
+		    else
+			{
+					input_error = true;
+					printf("the input is anomalous! Type of command wrong!\n");
+			}
+	
+		}
+		else
+		{
+			input_error = true;
+			printf("Numbers of command parameters are not correct!%d\n", argc);
+		}
+	}
+
+	char check::get_type()
+	{
+		return type;
+	}
+	int check::get_num()
+	{
+		return num;
+	}
+	char* check::get_filename()
+	{
+		return filename;
+	}
+	bool check::found_error()
+	{
+		return input_error;
+	}
---
+++ check.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<cstdlib>
+#include<string>
+using namespace std;
+ 
+class check
+{
+public:
+	check(int argc, char *argv[]);
+	void input_type_analyse();
+	char get_type();
+	int get_num();
+	char* get_filename();
+	bool found_error();
+
+private:
+	int argc;
+	char **argv;
+	int num;
+	char type;
+	char* filename;
+	bool input_error;
+};
